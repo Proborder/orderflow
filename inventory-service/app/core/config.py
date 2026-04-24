53f8b8b77d621de8859567dc446a4468987451c5
Maksim Seryakov <seryakov_2014@mail.ru>
2026-04-24 23:19:29 +0300

    feat: add inventory service

---
+++ inventory-service/app/core/config.py
@@ -0,0 +1,21 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    KAFKA_BOOTSTRAP_SERVERS_HOST: str
+    KAFKA_BOOTSTRAP_SERVERS_PORT: int
+
+    KAFKA_COMMAND_TOPIC: str
+    KAFKA_EVENTS_TOPIC: str
+
+    KAFKA_CONSUMER_TIMEOUT: int
+    KAFKA_CONSUMER_MAX_RECORDS: int
+
+    @property
+    def KAFKA_BOOTSTRAP_URL(self):
+        return f"{self.KAFKA_BOOTSTRAP_SERVERS_HOST}:{self.KAFKA_BOOTSTRAP_SERVERS_PORT}"
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
---
+++ inventory-service/app/schemas/messages.py
@@ -0,0 +1,22 @@
+import uuid
+from datetime import datetime
+from typing import Any
+
+from pydantic import BaseModel, Field
+
+
+class CommandMessage(BaseModel):
+    command_type: str
+    saga_id: uuid.UUID
+    order_id: uuid.UUID
+    payload: dict[str, Any]
+    message_id: uuid.UUID
+
+
+class EventMessage(BaseModel):
+    event_type: str
+    saga_id: uuid.UUID
+    order_id: uuid.UUID
+    payload: dict[str, Any]
+    message_id: uuid.UUID
+    timestamp: datetime
